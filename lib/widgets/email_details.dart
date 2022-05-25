1d0dcdb2a8224b682f53c3253b895916dbd63816
Deam <deam@aleph.engineering>
2022-05-25 12:22:10 -0400

    Refactor email details

--- lib/widgets/email_details.dart
+++ lib/widgets/email_details.dart
@@ -6,24 +6,36 @@ class EmailDetails extends StatelessWidget {
   const EmailDetails({
     Key? key,
     required this.email,
+    this.onPin,
+    this.pinned = false,
   }) : super(key: key);
 
   final Email email;
+  final Function()? onPin;
+  final bool pinned;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: pinned ? Colors.orange.withOpacity(.12) : Colors.white,
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.star_border),
+            onPressed: onPin,
+            icon: pinned
+                ? const Icon(
+                    Icons.star_sharp,
+                    color: Colors.orange,
+                  )
+                : const Icon(Icons.star_border),
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
@@ -40,15 +52,20 @@ class EmailDetails extends StatelessWidget {
                 EmailDeliveryInfo(email: email),
               ],
             ),
-            Text(
-              email.subject,
-              style: Theme.of(context).textTheme.headline6,
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                email.subject,
+                style: Theme.of(context).textTheme.headline6,
+              ),
             ),
             Expanded(
               child: SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(email.body),
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Text(
+                    email.body,
+                  ),
                 ),
               ),
             )
